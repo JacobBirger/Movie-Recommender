15671d501535ba86d5e9111a86867df5eeb99eba
jacobbirger <120600537+JacobBirger@users.noreply.github.com>
2025-10-25 14:31:46 -0500

    Add files via upload
    Added an autofill feature (still need to better understand the javascript aspects but that will come at a later date)
    Added a css file for better design
    Fixed issue where but -1 in the director input box would break the site

--- Movie_recomendation_website.py
+++ Movie_recomendation_website.py
@@ -1,8 +1,16 @@
-from flask import Flask, request, render_template, redirect, url_for
+#Current Project to-do list:
+#1. Make it so that you can click on any movie in the results and it will take you to a webpage that gives you more information about that movie
+#2. Make 
+
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from markupsafe import escape
 import pandas as pd
+
+#Creation of app taking template from the templates folder
+#Template is called template_1.html
 MovieRecomender = Flask(__name__,template_folder='templates')
 
+#Creating, cleaning, and sorting dataframe
 df=pd.read_csv('imdblist.csv')
 df_clean=df.drop(['Meta_score','Gross','Certificate'],axis=1)
 df_clean['Released_Year'].unique()
@@ -13,9 +21,27 @@ df_clean.rename(columns={df_clean.columns[13]:'Genre1'},inplace=True)
 df_clean.rename(columns={df_clean.columns[14]:'Genre2'},inplace=True)
 df_clean.rename(columns={df_clean.columns[15]:'Genre3'},inplace=True)
 
+@MovieRecomender.route('/suggest') #URL endpoint in flask app
+def suggest(): 
+    q=request.args.get('q','').strip().lower()
+    field=request.args.get('field','').strip().lower()
+    if not q or field not in {'director','actor','genre'}:
+        return jsonify([])
+    max_results=10
+    if field == 'director':
+        source = df_clean['Director'].dropna().astype(str).str.strip().str.lower().unique()
+    elif field == 'actor':
+        actor_cols = ['Star1', 'Star2', 'Star3', 'Star4']
+        source = pd.Series(pd.concat([df_clean[col].dropna().astype(str).str.strip() for col in actor_cols])).str.lower().unique()
+    else:
+        source = pd.Series(df_clean['Genre'].dropna().astype(str).str.split(',').explode()).str.strip().str.lower().unique()
+    matches=[item for item in source if q in item]
+    return jsonify([matches[:max_results]])
+
 @MovieRecomender.route('/', methods=['GET','POST'])
 def root():
     return redirect(url_for('Movie_Recomender'))
+
 @MovieRecomender.route('/recomendation', methods=['GET','POST'])
 def Movie_Recomender():
     results=[]
@@ -41,6 +67,10 @@ def Movie_Recomender():
                     'Star4':df_clean.iloc[i].Star4}
                     director_data.append(director_d)
             df_new=pd.DataFrame(director_data)
+        else:
+            df_new=df_clean[['Series_Title','IMDB_Rating','Overview','Genre','Star1','Star2','Star3','Star4']].copy()
+            df_new.rename(columns={df_new.columns[0]:'Title'},inplace=True)
+            df_new.rename(columns={df_new.columns[1]:'IMDB rating'},inplace=True)
         if genre != '-1':
             if ao1 == 'and':
                 while count < len(df_new):
